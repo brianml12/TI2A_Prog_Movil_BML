fe82f7790a483c11e5c2938af37df915244c3430
brianml12 <s20120168@alumnos.itsur.edu.mx>
2023-09-20 22:53:09 -0600

    Creacion de la topic de carta

--- Cuadricula_cursos/app/src/main/java/com/bsewamods/cuadricula_cursos/MainActivity.kt
+++ Cuadricula_cursos/app/src/main/java/com/bsewamods/cuadricula_cursos/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bsewamods.cuadricula_cursos.datos.DataSource
+import com.bsewamods.cuadricula_cursos.modelos.Topic
 import com.bsewamods.cuadricula_cursos.ui.theme.Cuadricula_CursosTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +35,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Grid(modifier: Modifier){
+    LazyVerticalGrid(GridCells.Fixed(2), )
+    {
+        items(DataSource.topics){
+            topic -> cartaTopic(topic)
+        }
+    }
+}
+
+@Composable
+fun cartaTopic(topic: Topic){
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Cuadricula_CursosTheme {
-        Greeting("Android")
+        //
     }
 }
