ea743fc4b20a845dd2736b06fe3576bf23d26c4a
brianml12 <s20120168@alumnos.itsur.edu.mx>
2023-09-21 19:19:07 -0600

    Creacion de tarjetas en dos filas

--- Cuadricula_cursos/app/src/main/java/com/bsewamods/cuadricula_cursos/MainActivity.kt
+++ Cuadricula_cursos/app/src/main/java/com/bsewamods/cuadricula_cursos/MainActivity.kt
@@ -3,16 +3,31 @@ package com.bsewamods.cuadricula_cursos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bsewamods.cuadricula_cursos.datos.DataSource
 import com.bsewamods.cuadricula_cursos.modelos.Topic
 import com.bsewamods.cuadricula_cursos.ui.theme.Cuadricula_CursosTheme
@@ -27,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TopicGrid(Modifier)
                 }
             }
         }
@@ -35,8 +50,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Grid(modifier: Modifier){
-    LazyVerticalGrid(GridCells.Fixed(2), )
+fun TopicGrid(modifier: Modifier){
+    LazyVerticalGrid(GridCells.Fixed(2))
     {
         items(DataSource.topics){
             topic -> cartaTopic(topic)
@@ -46,13 +61,55 @@ fun Grid(modifier: Modifier){
 
 @Composable
 fun cartaTopic(topic: Topic){
-
+    Card {
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.resImagen),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = topic.nombre),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                    )
+                    Text(
+                        text = topic.cursosdisponibles.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Cuadricula_CursosTheme {
-        //
+        val topic = Topic(R.string.fashion, 92, R.drawable.fashion)
+        Row {
+            cartaTopic(topic = topic)
+        }
     }
 }
