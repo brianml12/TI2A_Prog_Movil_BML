dca6ac234ecc544b226d3ccf6a7f991775a30f03
brianml12 <s20120168@alumnos.itsur.edu.mx>
2023-09-24 17:36:21 -0600

    App finalizada

--- App_superheroes/app/src/main/java/com/bsewamods/appsuperheroes/HeroesScreen.kt
+++ App_superheroes/app/src/main/java/com/bsewamods/appsuperheroes/HeroesScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -30,18 +28,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bsewamods.appsuperheroes.data.HeroesRepository
-import com.bsewamods.appsuperheroes.data.HeroesRepository.heroes
 import com.bsewamods.appsuperheroes.model.Hero
 import com.bsewamods.appsuperheroes.ui.theme.SuperheroesTheme
 
 class HeroesScreen {
 
     @Composable
-    fun ListHeroes(modifier: Modifier){
+    fun ListHeroes(contentPadding: PaddingValues = PaddingValues(0.dp)){
         val heroes: List<Hero> = HeroesRepository.heroes
         LazyColumn(
-            contentPadding = PaddingValues(0.dp)
-
+            contentPadding = contentPadding
         ){
             items(heroes) {
                 heroe -> tarjetaHeroe(heroe)
@@ -98,7 +94,7 @@ class HeroesScreen {
             Row {
                 tarjetaHeroe(hero)
             }*/
-            ListHeroes(Modifier);
+            ListHeroes();
         }
     }
 
--- App_superheroes/app/src/main/java/com/bsewamods/appsuperheroes/MainActivity.kt
+++ App_superheroes/app/src/main/java/com/bsewamods/appsuperheroes/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.bsewamods.appsuperheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +25,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HeroesScreen().ListHeroes(modifier = Modifier)
+                    SuperheroesApp()
                 }
             }
         }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SuperheroesApp() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar()
+            }
+        ) {
+            HeroesScreen().ListHeroes(contentPadding = it)
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            },
+            modifier = modifier
+        )
     }
 }
